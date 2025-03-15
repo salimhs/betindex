080f28b47f76bd5a7f9044116bfe1a788bb378c2
Masoud Karimi <74917502+MasoudKarimi4@users.noreply.github.com>
2025-03-15 18:24:30 -0400

    p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,17 @@
 
 //
 import MainPage from "../comps/main"
+import GamesDashboard from "../comps/gamesdashboard"
 
 export default function BettingDashboard() {
 
   return (
+    <div>
     <MainPage />
+
+    <GamesDashboard />
+
+    </div>
   )
 }
 
